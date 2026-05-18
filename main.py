c705dfc9ae002710f1f58b2f262c0e46727b14d0
Aman Mishra <aman@getnitro.co>
2026-05-18 15:20:50 +0530

    ap scheduler added

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from loguru import logger
 from fastapi import FastAPI
 from api import product
+
+scheduler = AsyncIOScheduler()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Application started successfully")
+    #$scheduler.add_job("", trigger="interval", minutes=5, max_instances=1)
+    #scheduler.start()
+    logger.info("Scheduler started")
+
+    yield
+    scheduler.shutdown()
+    logger.info("Scheduler stopped!!")
+
 
 app = FastAPI(
     title="Noon Scrapper",
     description="Noon scrapper service that scrappes product, category, brands, store, etc.",
-    version='1.0.1'
+    version='1.0.1',
+    lifespan=lifespan
     )
 
 
