e621cb08010cf95dbfedb97b5451de8eb651134c
Aman Mishra <aman@getnitro.co>
2026-05-18 18:36:21 +0530

    category added

---
+++ workers/category.py
@@ -0,0 +1,34 @@
+import asyncio
+from loguru import logger
+
+import utils.dramatiq
+from scrappers.category import NoonCategoryScrapper
+from controllers.brand import NoonBrandService
+from utils.db import SessionLocal
+
+
+# @dramatiq.actor(max_retries=3)
+def scrape_noon_categories():
+    logger.info("Brand scrapping is started....")
+    asyncio.run(run_scraper())
+
+
+async def run_scraper():
+    scraper = NoonCategoryScrapper()
+    try:
+        brands = await scraper.fetch_all_brands()
+        logger.info(f"Total brands fetched are: {len(brands)}")
+        unique_brands = scraper.deduplicate(brands)
+
+        db = SessionLocal()
+
+        try:
+            for brand in unique_brands:
+                logger.info(f'Adding {brand} in the database')
+                await NoonBrandService.create(db, name=brand)
+
+        except Exception as err:
+            logger.exception(f'Error occurred while creating entry in database')
+
+    finally:
+        await scraper.proxy_client.close_all_sessions()
