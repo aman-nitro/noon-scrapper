424dc17c388e069fbaef6c0a673a9e44d1002929
Aman Mishra <aman@getnitro.co>
2026-05-18 17:42:48 +0530

    brand model fixes

--- models/brand.py
+++ models/brand.py
@@ -1,19 +1,24 @@
 from models.base import Base
+from datetime import datetime
 from sqlalchemy import Column, DateTime, Text, func, Integer
-
 
 class NoonBrand(Base):
     __tablename__ = "noon_brand"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(Text,nullable=False)
-    createdAt = Column(DateTime,nullable=False,server_default=func.now())
-    updatedAt = Column(DateTime,nullable=False, server_default=func.now())
+    name = Column(Text, nullable=False)
 
-    def __repr__(self):
-        return (
-            f"<NoonBrand("
-            f"id={self.id}, "
-            f"name={self.name}"
-            f")>"
-        )
+    createdAt = Column(
+        DateTime,
+        nullable=False,
+        server_default=func.now(),
+        default=datetime.utcnow
+    )
+
+    updatedAt = Column(
+        DateTime,
+        nullable=False,
+        server_default=func.now(),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
